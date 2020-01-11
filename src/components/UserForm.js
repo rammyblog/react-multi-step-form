bef650085fd7e6226fcb9731b8372cdb8992bc37
Onasanya Tunde <Onasanyatunde67@gmail.com>
2020-01-12 00:55:14 +0100

    Done without validations

---
+++ src/components/UserForm.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import FormUserDetails from './FormUserDetails';
+import FormPersonalDetails from './FormPersonalDetails';
+import Confirm from './Confirm';
+import Success from './Success';
+
+
+export class UserForm extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            step: 1,
+            firstName: '',
+            lastName: '',
+            email: '',
+            occupation: '',
+            city: '',
+            bio: ''
+
+        };
+    }
+
+    // Proceed to next step
+
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step : step + 1
+        })
+    }
+
+
+    // go back to prev step
+    prevStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step : step - 1
+        })
+    }
+
+    // Handle fields change
+
+    handleChange = input => e => {
+        this.setState({
+            [input]: e.target.value
+        })
+    }
+
+    render() {
+        const { step } = this.state;
+        const { firstName, lastName, email, occupation, city, bio} = this.state;
+        const values = { firstName, lastName, email, occupation, city, bio}
+
+        switch (step) {
+            case 1:
+                return (
+                    <FormUserDetails 
+                        nextStep = {this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+            case 2:
+                return <FormPersonalDetails
+                            prevStep = {this.prevStep}
+                            nextStep = {this.nextStep}
+                            handleChange={this.handleChange}
+                            values={values}
+                        />
+            
+            case 3:
+                return <Confirm 
+                        prevStep = {this.prevStep}
+                        nextStep = {this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+            case 4:
+                return <Success />
+            default:
+                break;
+        }
+    
+    }
+}
+
+export default UserForm
